ba06173cdd852e2eaee3433f3e0ad5b75978b071
simtimj <juliussimtim@gmail.com>
2019-05-10 09:49:36 -0700

    Init.

---
+++ README.txt
@@ -0,0 +1,9 @@
+
+Description:
+AscDescTimeBoxing An application that allows the user to set a goal, the tasks and events that lead up to it, 
+and set an amount of time (time box) per task/events.
+
+Start App:
+script for starting server;
+script for building webpack;
+script for seeding with mock data;
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+  console.log(`Listening on locahost:${port}`)
+})
